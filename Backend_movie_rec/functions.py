a8afb04dff8c8de8c8e3f24120f68ce416e90a3a
abhigeddam <92228141+abhigeddam@users.noreply.github.com>
2024-08-26 16:56:21 +0530

    Update functions.py
    removed key

--- Backend_movie_rec/functions.py
+++ Backend_movie_rec/functions.py
@@ -7,7 +7,7 @@ def req_data(data):
   url = "https://api.themoviedb.org/3/movie/"+str(data)+"?language=en-US"
   headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTFhZWYxZDQ3OWE4ZWE4MDA2YjEyNWVhM2MwNTlkMCIsIm5iZiI6MTcyMzY3NTQxNC4wODc1NjQsInN1YiI6IjY1ZGYxZjE3OGU4NzAyMDE4NDA0ZTA3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mAk46DbbGYJkHdOi3aWb4kQfFzIp3MYWFXyZ07-oNLE"
+    "Authorization": "Bearer"
     }
   response = requests.get(url, headers=headers).json()
   print(response)
